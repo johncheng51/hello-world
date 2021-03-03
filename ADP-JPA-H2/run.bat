917d1379b6bf759317395a6c90f2a80e02fc8905
john <johnchengvzw@gmail.com>
2021-03-03 10:40:24 -0500

    211111212

--- ADP-JPA-H2/run.bat
+++
@@ -1,1 +0,0 @@
-java -jar target/ADP-JPA-H2-0.0.1-SNAPSHOT.jar
--- ADP-JPA-H2/src/main/java/com/jm/repository/ProjectDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jm.repository;
-import com.jm.domain.ProjectDetailsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository 
-public interface ProjectDetailsRepository extends JpaRepository<ProjectDetailsEntity, Long> {
-     public ProjectDetailsEntity findOneByProjectName(String name);
-}
